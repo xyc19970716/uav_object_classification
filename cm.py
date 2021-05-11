7b1e15e1c94ce27c4937ce7380bb0061a1eb4c65
xyc19970716 <465140038@qq.com>
2021-05-11 14:09:34 +0800

    add eval code.

---
+++ cm.py
@@ -0,0 +1,170 @@
+# _*_ coding: utf-8 _*_
+
+import sys
+import os
+import glob
+import io
+import cv2
+import numpy as np
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')  # 打印出中文字符
+try:
+    from osgeo import gdal
+    from osgeo import ogr
+    from osgeo import osr
+except ImportError:
+    import gdal
+    import ogr
+    import osr
+
+from PIL import Image
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+def world2Pixel(geoMatrix, x, y):
+    """
+    Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
+    the pixel location of a geospatial coordinate
+    """
+    ulX = geoMatrix[0]
+    ulY = geoMatrix[3]
+    xDist = geoMatrix[1]
+    pixel = int((x - ulX) / xDist)
+    line = int((ulY - y) / xDist)
+    return (pixel, line)
+class ConfusionMatrix(object):
+    
+
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))#初始化混淆矩阵，元素都为0
+        self.num_classes = num_classes#类别数量，本例数据集类别为5
+        self.labels = labels#类别标签
+
+    def update(self, preds, labels):
+        # for p, t in zip(preds, labels):#pred为预测结果，labels为真实标签
+            # self.matrix[p, t] += 1#根据预测结果和真实标签的值统计数量，在混淆矩阵相应位置+1
+        self.matrix[preds, labels] += 1
+    def summary(self):#计算指标函数
+        # calculate accuracy
+        sum_TP = 0
+        n = np.sum(self.matrix)
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]#混淆矩阵对角线的元素之和，也就是分类正确的数量
+        acc = sum_TP / n#总体准确率
+        print("the model accuracy is ", acc)
+		
+		# kappa
+        sum_po = 0
+        sum_pe = 0
+        for i in range(len(self.matrix[0])):
+            sum_po += self.matrix[i][i]
+            row = np.sum(self.matrix[i, :])
+            col = np.sum(self.matrix[:, i])
+            sum_pe += row * col
+        po = sum_po / n
+        pe = sum_pe / (n * n)
+        # print(po, pe)
+        kappa = round((po - pe) / (1 - pe), 3)
+        #print("the model kappa is ", kappa)
+        
+        # precision, recall, specificity
+        table = PrettyTable()#创建一个表格
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):#精确度、召回率、特异度的计算
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.#每一类准确度
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+        return str(acc)
+
+    def plot(self):#绘制混淆矩阵
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # 设置x轴坐标label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # 设置y轴坐标label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix (acc='+self.summary()+')')
+
+        # 在图中标注数量/概率信息
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                # 注意这里的matrix[y, x]不是matrix[x, y]
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+
+classes = ['0', '1', '2', '3', '4']
+        #    1      2       3       4       5       6       7       8
+print([classes[i] for i in range(len(classes))])
+
+Mutli_Tem_Images_Path = r'D:\classification\data\val'#'../../data/processed'
+Mutli_Tem_Images_Paths = os.listdir(Mutli_Tem_Images_Path)
+
+
+
+
+print(Mutli_Tem_Images_Paths)
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+trans = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+model = models.__dict__["resnet18"]()
+model = torch.nn.DataParallel(model).cuda()
+checkpoint = torch.load('./model_best.pth.tar')
+model.load_state_dict(checkpoint['state_dict'])
+model.eval()
+
+
+
+confusion = ConfusionMatrix(num_classes=len(classes), labels=classes)
+for i, type_path in enumerate(Mutli_Tem_Images_Paths):
+    paths = glob.glob(os.path.join(Mutli_Tem_Images_Path, type_path, '*.tif'))
+    for p in tqdm(paths):
+        srcImage = gdal.Open(p)
+        geoTrans = srcImage.GetGeoTransform()
+        img = srcImage.ReadAsArray()   
+        img = np.transpose(img, (1,2,0))   
+        showImage = Image.fromarray(img)# Image.fromarray(cv2.cvtColor(showImage,cv2.COLOR_BGR2RGB)) 
+        input_tensor = trans(showImage)
+        input_tensor = input_tensor.unsqueeze(0)
+        with torch.no_grad():
+            output = model(input_tensor)
+            pred_y = torch.max(output,1)[1]
+            pred_y_data = pred_y.detach().cpu().numpy() 
+            confusion.update(pred_y_data[0], int(type_path))
+
+
+
+confusion.plot()
+# confusion.summary()
